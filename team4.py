46714ed688e6de229da978bdc45f0988ddfdcc93
Kevin <ktnguy20@gmail.com>
2019-03-08 13:33:01 -0500

    Update team4.py

--- team4.py
+++ team4.py
@@ -1,25 +1,10 @@
-team_name = 'Thot Patrol' # Only 10 chars displayed.
-strategy_name = 'Exterminate all thots'
-strategy_description = 'Betray if they colluded last move'
+team_name = 'ThotPatrol'
+strategy_name = 'Determine by recent move'
+strategy_description = 'For the first move collude, then if my last move is b return b if their last move was c, else return c. If my last move was collude then if their last move was collude then collude else betray.'
 
 def move(my_history, their_history, my_score, their_score):
-    ''' Arguments accepted: my_history, their_history are strings.
-    my_score, their_score are ints.
-    
-    Make my move.
-    Returns 'c' or 'b'. 
-    '''
-
-    # my_history: a string with one letter (c or b) per round that has been played with this opponent.
-    # their_history: a string of the same length as history, possibly empty. 
-    # The first round between these two players is my_history[0] and their_history[0].
-    # The most recent round is my_history[-1] and their_history[-1].
-    
-    # Analyze my_history and their_history and/or my_score and their_score.
-    # Decide whether to return 'c' or 'b'.
-    
     if len(my_history) == 0:
-        return 'b'
+        return 'c'
     else:
         if my_history[-1] == 'b':
             if their_history[-1] == 'c':
@@ -28,6 +13,6 @@ def move(my_history, their_history, my_score, their_score):
                 return 'c'
         else:
             if their_history[-1] == 'c':
-                return 'b'
-            else:
                 return 'c'
+            else:
+                return 'b'

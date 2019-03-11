663a09d32dd5ad8cb65036eda246f710416c0457
Jayden C <jlcurt19@>
2019-03-11 13:28:34 -0400

    Update team5.py

--- team5.py
+++ team5.py
@@ -6,9 +6,11 @@
 #     move: A function that returns 'c' or 'b'
 ####
 
-team_name = 'The name the team gives to itself' # Only 10 chars displayed.
-strategy_name = 'The name the team gives to this strategy'
-strategy_description = 'How does this strategy decide?'
+import random
+
+team_name = 'ChaosGblns' # Only 10 chars displayed.
+strategy_name = 'The Schizophrenic Statistician'
+strategy_description = 'Randomized Probability'
     
 def move(my_history, their_history, my_score, their_score):
     ''' Arguments accepted: my_history, their_history are strings.
@@ -26,8 +28,11 @@ def move(my_history, their_history, my_score, their_score):
     # Analyze my_history and their_history and/or my_score and their_score.
     # Decide whether to return 'c' or 'b'.
     
-    return 'c'
-
+    x=random.randint(65,95)/100.
+    if random.random()<=x:
+        return 'b'
+    else:
+        return 'c'
     
 def test_move(my_history, their_history, my_score, their_score, result):
     '''calls move(my_history, their_history, my_score, their_score)
